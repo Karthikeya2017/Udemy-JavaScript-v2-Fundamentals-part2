bd0d28f4d46b1b771a14b91d204bb9f24ddb0637
C61353 <gopikrishna.pentyala@nl.abnamro.com>
2020-11-20 17:11:15 +0100

    challenge3

--- Loops/example.js
+++ Loops/example.js
@@ -1,6 +1,23 @@
-let dice = 0 ;
+// let dice = 0 ;
 
-while (dice !== 6) {
-    dice = Math.trunc(Math.random() * 6 ) + 1;
-    console.log(`dice value ${dice}`);
+// while (dice !== 6) {
+//     dice = Math.trunc(Math.random() * 6 ) + 1;
+//     console.log(`dice value ${dice}`);
+// }
+
+const bills = [22, 297, 76, 440, 37, 105, 10, 1100, 86, 82];
+const tips = [ ];
+const totalAmount = [ ];
+
+const calcTipsAndTotal = (billsArray) => {
+    billsArray.forEach(bill => {
+        const tip = (50 < bill < 200)? bill * 0.20: bill * 0.10;
+        total = bill + tip;
+        tips.push(tip);
+        totalAmount.push(total);
+    });
 }
+
+calcTipsAndTotal(bills);
+console.log(totalAmount);
+console.log(tips);
